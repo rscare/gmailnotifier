a0c7823130d58e1cce231bcc193458a54e6ce6fa
Yuri D. Lenskiy <yuri.lenskiy@gmail.com>
2011-09-27 21:56:27 -0400

    Changed gmailnotifier to use librpass.

--- gmailnotifier.c
+++ gmailnotifier.c
@@ -1,5 +1,9 @@
-#include "rpass.h"
 #include "gmailxml.h"
+#include <rpass.h>
+
+#include <sys/socket.h>
+#include <sys/un.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,7 +18,7 @@
 #include <libxml/parser.h>
 
 #define URL "https://mail.google.com/mail/feed/atom"
-#define PASSFILE "/home/redscare/.passwords.gpg"
+#define PASSFILE "/home/redscare/.rpasswords"
 #define WAITTIME 7 // How long to wait between tries
 #define MAXTRIES 7
 #define NOTIFICATION_SLEEP 180 // How long to wait between notifications
@@ -82,30 +86,110 @@ void SetupSignals() {
     sigaction(SIGTERM, &sa, NULL);
 }
 
+void ConstructDaemonMsg(void *msg, int nargs, ...) {
+    va_list argp;
+    void *msg_ptr = msg, *tmp;
+    int i;
+    size_t tmp_size;
+    va_start(argp, nargs);
+
+    for (i = 0; i < nargs; ++i) {
+        tmp = va_arg(argp, void *);
+        tmp_size = va_arg(argp, size_t);
+
+        memcpy(msg_ptr, tmp, tmp_size);
+        msg_ptr += tmp_size;
+        if (i < nargs - 1)
+            *(char *)(msg_ptr++) = ' ';
+    }
+
+    va_end(argp);
+}
+
 int GetUSERPWD(char **str, const char * const acname) {
-    gpgme_data_t ptext = NULL;
-    rpass_parent *rparent = NULL;
-    rpass_entry *uentry = NULL, *pentry = NULL;
-    MemoryStruct *mem;
+    int s, len, in_len, remaining;
+    int flags = 0;
+    struct sockaddr_un remote;
 
-    gpgme_data_new(&ptext);
-    initialize_engine();
+    void *msg, *msg_ptr;
 
-    decrypt_file(PASSFILE, ptext);
-    GetAccountInfo(ptext, acname, &rparent);
+    char tmp[BUFSIZ], *user, *pass;
 
-    pentry = uentry = rparent->first_entry;
-    while (strcmp(uentry->key, "user") != 0)
-        uentry = uentry->next_entry;
-    while (strcmp(pentry->key, "pass") != 0)
-        pentry = pentry->next_entry;
+    rpass_parent *p; rpass_entry *e;
 
-    *str = (char *)malloc(strlen(uentry->value) + strlen(pentry->value) + 2);
-    sprintf(*str, "%s:%s", uentry->value, pentry->value);
+    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        exit(1);
+    }
 
-    destroy_rpass_parent(rparent);
-    destroy_engine();
-    gpgme_data_release(ptext);
+    remote.sun_family = AF_UNIX;
+    strcpy(remote.sun_path, SOCKET_NAME);
+    len = strlen(remote.sun_path) + sizeof(remote.sun_family);
+    if (connect(s, (struct sockaddr *)&remote, len) == -1) {
+        exit(1);
+    }
+
+    len = strlen(RPASS_DAEMON_MSG_GETACCOUNTS)
+        + 1 + strlen(PASSFILE)
+        + 1 + strlen(RPASS_DAEMON_AC_START)
+        + 1 + strlen(acname)
+        + 1 + strlen(RPASS_DAEMON_AC_START)
+        + 1 + sizeof(int)
+        + 1 + strlen("user")
+        + 1 + strlen("pass");
+
+    msg = malloc(len);
+
+    ConstructDaemonMsg(msg, 8,
+                       RPASS_DAEMON_MSG_GETACCOUNTS, strlen(RPASS_DAEMON_MSG_GETACCOUNTS),
+                       PASSFILE, strlen(PASSFILE),
+                       RPASS_DAEMON_AC_START, strlen(RPASS_DAEMON_AC_START),
+                       acname, strlen(acname),
+                       RPASS_DAEMON_AC_START, strlen(RPASS_DAEMON_AC_START),
+                       &flags, sizeof(int),
+                       "user", strlen("user"),
+                       "pass", strlen("pass"));
+
+    send(s, msg, len, 0);
+    free(msg);
+
+    msg_ptr = msg = malloc(BUFSIZ + 1);
+    len = BUFSIZ + 1;
+    remaining = BUFSIZ;
+
+    while (in_len = recv(s, tmp, BUFSIZ, MSG_WAITALL)) {
+        remaining -= in_len;
+        if (remaining < 0) {
+            len += BUFSIZ;
+            remaining += BUFSIZ;
+            msg = realloc(msg, len);
+        }
+        memcpy(msg_ptr, tmp, in_len);
+        msg_ptr += in_len;
+        if (in_len < BUFSIZ) {
+            break;
+        }
+    }
+
+    *(char *)(++msg_ptr) = '\0';
+
+    createRpassParentFromString(&p, msg);
+    free(msg);
+
+    e = p->first_entry;
+    while(e) {
+        if (strcmp(e->key, "user") == 0) {
+            user = e->value;
+        }
+        else if (strcmp(e->key, "pass") == 0) {
+            pass = e->value;
+        }
+        e = e->next_entry;
+    }
+
+    *str = malloc(strlen(user) + 1 + strlen(pass) + 1);
+    sprintf(*str, "%s:%s", user, pass);
+
+    freeRpassParents(p);
 
     return 0;
 }
